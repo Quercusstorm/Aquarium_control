d4e76925c6f77fac84ab8c79b8bff3f549431360
Quercusstorm <74053004+Quercusstorm@users.noreply.github.com>
2022-02-18 23:25:22 +0100

    Initial commit

---
+++ Aquarium_control/main.c
@@ -0,0 +1,18 @@
+/*
+ * Aquarium_control.c
+ *
+ * Created: 18.02.2022 22:49:56
+ * Author : vitve
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
