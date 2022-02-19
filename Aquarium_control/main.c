408df5790cb6acd76f75c2b5a074e1c1ee18b187
Quercusstorm <74053004+Quercusstorm@users.noreply.github.com>
2022-02-19 13:24:31 +0100

    Basic PWM
    First PWM function testing, D6 LED PWM blink.

--- Aquarium_control/main.c
+++ Aquarium_control/main.c
@@ -1,19 +1,20 @@
-/*
- * Aquarium_control.c
- *
- * Created: 18.02.2022 22:49:56
- * Author : vitve
- */ 
-
+#define F_CPU 16000000
 #include <avr/io.h>
+#include <util/delay.h>
 
-
-int main(void)
-{
-	int a;
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+int main(void) {
+	DDRD |= (1<<PD6);									//pin D6 is OUTPUT
+	TCCR0A |= (1<<COM0A1) | (1<<WGM00) | (1<<WGM01);	//COM0A1 is settings of "compare output mode", WGM is setting of "PWM mode"
+	TCCR0B |= (1<<CS00);								//Prescaling settings
+	while (1) {											
+	
+		for (int a=0; a<256; a++) {
+			OCR0A = a;									//add numbers to OCR0A (edit PWM value)
+			_delay_ms(10);
+		}
+		for (int a=0; a<256; a++) {						//sub numbers OCR0A (edit PWM value)
+			OCR0A = 255-a;
+			_delay_ms(10);
+		}
+	}
 }
-
